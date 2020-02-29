5c010d77bf325fdc3683273ee9fd0ea61c65d233
Riccardo <Coppola>
2020-02-29 19:06:38 +0000

    Adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Riccardo Coppola`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `About me`,
     author: `hello@ricca509.me`,
   },
   plugins: [
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `favicon.png`,
       },
     },
     `gatsby-transformer-remark`,

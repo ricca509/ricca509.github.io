08a4e0478c84d53b75d509d940a5e64bbfd5aba8
Riccardo <Coppola>
2020-04-08 19:56:33 +0100

    Fixes hook for server-side rendering in Gatsby

--- src/hooks/useIsPrinting/index.js
+++ src/hooks/useIsPrinting/index.js
@@ -3,10 +3,10 @@ import { useState, useEffect } from "react";
 const useIsPrinting = () => {
   const [isPrinting, setIsPrinting] = useState(false);
 
-  const printModeMq =
-    window !== undefined && window.matchMedia && window.matchMedia("print");
-
   useEffect(() => {
+    const printModeMq =
+      window !== undefined && window.matchMedia && window.matchMedia("print");
+
     const handlePrintModeChange = () => {
       setIsPrinting(printModeMq && Boolean(printModeMq.matches));
     };

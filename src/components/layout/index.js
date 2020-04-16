eb7b20a420deafc758e05672ec7d69e1854b2c66
Riccardo <Coppola>
2020-04-16 13:43:59 +0100

    Adds story for layout file

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,24 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
 import Header from "../header";
 import Footer from "../footer";
 import "./layout.css";
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <div className="container">
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header />
       <div>
         <main>{children}</main>
         <Footer />

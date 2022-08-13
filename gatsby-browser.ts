f0d433683df3166eb011ed6ad8656f6801b7c5b3
Riccardo Coppola <ricca509@gmail.com>
2022-08-13 16:44:16 +0200

    Change casing

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -3,7 +3,7 @@ import "@fontsource/inconsolata/700.css";
 import "@fontsource/poppins/300.css";
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/700.css";
-import "@fontsource/Roboto/400.css";
+import "@fontsource/roboto/400.css";
 
 // normalize CSS across browsers
 import "./src/normalize.css";
